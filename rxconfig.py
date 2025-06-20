5218459df391ecc1b0c9742f843ea25406607965
Daniel <pipidepulus@hotmail.com>
2025-06-20 12:24:19 -0500

    feat: Set production api_url for Render

--- rxconfig.py
+++ rxconfig.py
@@ -2,6 +2,7 @@ import reflex as rx
 
 config = rx.Config(
     app_name="asistente_legal_constitucional_con_ia",
+    api_url="https://legalcolrag.onrender.com",
     plugins=[rx.plugins.TailwindV3Plugin()],
     frontend_packages=[
         "@clerk/localizations",
