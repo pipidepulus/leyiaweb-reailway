ac4aa38b6e004ad91f6971b30e1de7089b0f93ea
Daniel <pipidepulus@hotmail.com>
2025-06-20 14:32:25 -0500

    feat: Restore full application with clean code and final config

--- rxconfig.py
+++ rxconfig.py
@@ -4,4 +4,8 @@ import reflex as rx
 config = rx.Config(
     app_name="asistente_legal_constitucional_con_ia",
     api_url="https://legalcolrag.onrender.com",
+    frontend_packages=[
+        "@chakra-ui/system",
+        "chakra-react-select",
+    ],
 )
