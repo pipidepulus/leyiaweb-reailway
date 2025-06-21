054856e432b5a8a84cba166091e1a8e42d67b6ed
Daniel <pipidepulus@hotmail.com>
2025-06-21 11:45:27 -0500

    feat: Añadir api_url para despliegue en Render

--- rxconfig.py
+++ rxconfig.py
@@ -3,6 +3,7 @@ import reflex as rx
 
 config = rx.Config(
     app_name="asistente_legal_constitucional_con_ia",
+    api_url="https://legalcolrag.onrender.com",
     plugins=[
         rx.plugins.TailwindV3Plugin(),
     ],
