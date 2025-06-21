61872bac278da2792d607ae691c6d1ed9fdc7641
Daniel <pipidepulus@hotmail.com>
2025-06-20 21:22:46 -0500

    feat: Apply workaround to force WebSocket URL in AppState

--- rxconfig.py
+++ rxconfig.py
@@ -3,12 +3,11 @@ import reflex as rx
 
 config = rx.Config(
     app_name="asistente_legal_constitucional_con_ia",
-    api_url="https://legalcolrag.onrender.com",
     plugins=[
         rx.plugins.TailwindV3Plugin(),
     ],
     frontend_packages=[
-        #"@clerk/localizations",
+        "@clerk/localizations",
         "@chakra-ui/system",
         "chakra-react-select",
     ],
