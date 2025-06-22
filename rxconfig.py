c2a75d9402112f2a6b726416add86a18a43060d2
Daniel <pipidepulus@hotmail.com>
2025-06-21 23:19:29 -0500

    Update Dockerfile

--- rxconfig.py
+++ rxconfig.py
@@ -2,6 +2,6 @@
 import reflex as rx
 
 config = rx.Config(
-    app_name="tu_app",
+    app_name="asistente_legal_constitucional_con_ia",
     api_url="http://localhost:8000", # Esto será sobreescrito en producción
 )
