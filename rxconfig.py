f52e3155b93b71220bc3c5046305e708913f77ca
Daniel <pipidepulus@hotmail.com>
2025-06-28 20:19:12 -0500

    feat: Configurar explícitamente el plugin de Tailwind

--- rxconfig.py
+++ rxconfig.py
@@ -5,8 +5,6 @@ config = rx.Config(
     app_name="asistente_legal_constitucional_con_ia",
     api_url="http://localhost:8000", # Esto será sobreescrito en producción
     show_built_with_reflex=False,
-    plugins=[
-        tailwind.TailwindV3Plugin(), 
-    ],
+    tailwind={},
 )
 
